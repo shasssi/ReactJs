b832e5f02e5cb13ae8187d66361f878033c410e7
shasssi <59342317+shasssi@users.noreply.github.com>
2024-03-25 22:02:31 +0530

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
