65e59c72fc03a8ba936dbde73f5ffefb7d0b84bb
shasssi <59342317+shasssi@users.noreply.github.com>
2024-04-06 23:51:08 +0530

    lazy loading content

---
+++ src/components/Lazy/LazyComponent.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import useStyles from "../LifeCycleMethods/LifeCycleMethods.style";
+import PreFormatComponent from "../common/PreFormat/PreFormatComponent";
+import { renderContentListData } from "../utilityComponent";
+import {
+  lazyLoadinData,
+  lazyLoadinCode,
+  suspenseData,
+} from "./lazyLoadingConfig";
+
+function ReactComponents(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Typography variant="button" className={classes.topHeading}>
+        Lazy Loading - On Demand Loading
+      </Typography>
+      {renderContentListData(lazyLoadinData)}
+      {renderContentListData(suspenseData)}
+      <PreFormatComponent data={lazyLoadinCode} />
+    </div>
+  );
+}
+
+export default ReactComponents;
