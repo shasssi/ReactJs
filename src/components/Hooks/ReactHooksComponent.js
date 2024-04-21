2a462b016e55107744d0596a6cc814b8d739f4b7
shasssi <59342317+shasssi@users.noreply.github.com>
2024-04-21 17:36:44 +0530

    react hooks content

---
+++ src/components/Hooks/ReactHooksComponent.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import useStyles from "../LifeCycleMethods/LifeCycleMethods.style";
+import PreFormatComponent from "../common/PreFormat/PreFormatComponent";
+import { renderContentListData } from "../utilityComponent";
+import {
+  uncontrolledComponentCode,
+  uncontrolledComponentData,
+  hooksData,
+  useRefData,
+  useRefCode,
+  useCallbackData,
+  parentComponentCode,
+  childComponentCode,
+  useMemoData,
+  useMemoCode,
+  useContextData,
+  createContextCode,
+  nestedComponentCode,
+  useContextCode,
+} from "./reactHooksConfig";
+
+function ReactComponents(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Typography variant="button" className={classes.topHeading}>
+        React Hooks
+      </Typography>
+      {renderContentListData(hooksData)}
+      {renderContentListData(uncontrolledComponentData)}
+      <PreFormatComponent data={uncontrolledComponentCode} />
+      {renderContentListData(useRefData)}
+      <PreFormatComponent data={useRefCode} />
+      <Typography variant="button" className={classes.topHeading}>
+        Performance optimization
+      </Typography>
+      {renderContentListData(useCallbackData)}
+      <PreFormatComponent data={parentComponentCode} />
+      <PreFormatComponent data={childComponentCode} />
+      <Typography variant="button" className={classes.topHeading}>
+        Performance optimization
+      </Typography>
+      {renderContentListData(useMemoData)}
+      <PreFormatComponent data={useMemoCode} />
+      {renderContentListData(useContextData)}
+      <PreFormatComponent data={createContextCode} />
+      <PreFormatComponent data={nestedComponentCode} />
+      <PreFormatComponent data={useContextCode} />
+    </div>
+  );
+}
+
+export default ReactComponents;
